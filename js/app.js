bf6b4ebb0ae63f53d55be340e5d1fa863c1b03ab
MrMargouillat <klaja.raphael@gmail.com>
2016-10-09 20:39:31 +0200

    Fix app.js

--- js/app.js
+++ js/app.js
@@ -2,5 +2,5 @@ var navBtn  = document.querySelector('#header__language')
 navBtn.addEventListener("click", function(e){
     e.preventDefault();
     console.log(document.querySelector('body'));
-    document.querySelector('body').classList.toggleClass('with--sidebar');
+    document.querySelector('body').classList.toggle('with--sidebar');
 });
