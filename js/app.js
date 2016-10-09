ac373c2f291dc7d7105bcd419eb70aa8c714ca8a
MrMargouillat <klaja.raphael@gmail.com>
2016-10-09 16:30:25 +0200

    Use native js and some other stuff

---
+++ js/app.js
@@ -0,0 +1,6 @@
+var navBtn  = document.querySelector('#header__language')
+navBtn.addEventListener("click", function(e){
+    e.preventDefault();
+    console.log(document.querySelector('body'));
+    document.querySelector('body').classList.toggleClass('with--sidebar');
+});
